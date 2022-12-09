ac354887f2fe486eadef5d03ac3f87d3882bce1d
PapaZrix <zrilic313@gmail.com>
2022-12-09 11:08:01 +0100

    Add functions for rendering appropriate gameboard in displayController object

--- script.js
+++ script.js
@@ -24,7 +24,65 @@ const Player = (name, sign) => {
 }
 
 const displayController = (() => {
+    const playBtn1 = document.querySelector(".two-btn");
+    const playBtn2 = document.querySelector(".ai-btn");
+    const gameBoard1 = document.querySelector(".game-board");
+    const gameBoard2 = document.querySelector(".game-board-ai");
+    const gameMessage = document.querySelector(".game-message");
+    const restartBtn = document.querySelector(".restart-button");
+    const backBtn = document.querySelector(".back");
+    const playGameDiv = document.querySelector(".play-game");
+    const twoPlayerCells = document.querySelectorAll(".board-cell");
+    const boardCellsAi = document.querySelectorAll(".boad-cell-ai");
 
+    // Initial display > just the mode choice
+    gameBoard1.style.display = "none";
+    gameBoard2.style.display = "none";
+    gameMessage.style.display = "none";
+    restartBtn.style.display = "none";
+    backBtn.style.display = "none";
+
+    // Set up DOM for 2 player game
+    const setGameDisplayTwoPlayers = () => {
+        playGameDiv.style.display = "none";
+        gameMessage.style.display = "block";
+        gameBoard1.style.display = "grid";
+        gameBoard2.style.display = "none";
+        restartBtn.style.display = "flex";
+        backBtn.style.display = "flex";
+    }
+
+    // Set up DOM for player vs AI
+    const setGameDisplayWithAi = () => {
+        playGameDiv.style.display = "none";
+        gameMessage.style.display = "block";
+        gameBoard1.style.display = "none";
+        gameBoard2.style.display = "grid";
+        restartBtn.style.display = "flex";
+        backBtn.style.display = "flex";
+    }
+
+    const renderGameBoard = () => {
+        for (let i = 0; i < twoPlayerCells.length; i++) {
+            twoPlayerCells[i].innerHTML = gameBoard.getBoardCell(i);
+        }
+    }
+
+    const renderAiGameBoard = () => {
+        for (let i = 0; i < boardCellsAi.length; i++) {
+            boardCellsAi[i].innerHTML = gameBoard.getBoardCell(i);
+        }
+    }
+
+    const displayWinnerText = (winner) => {
+        gameMessage.textContent = winner;
+    }
+
+    const displayTurnUpdate = (message) => {
+        gameMessage.textContent = message;
+    }
+
+    return {displayTurnUpdate, displayWinnerText};
 })();
 
 const gameController = (() => {
